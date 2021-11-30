98e8b6b7bc85b9eb8730f8ddc02b866ee89a6378
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2021-11-30 17:53:42 -0300

    Add files via upload

---
+++ scr/database/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+try {
+ let connect =  mongoose.connect(
+    "mongodb+srv://teste:Password@cluster0.3ecyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+
+  );
+  module.exports = connect; 
+} catch (err) {
+  console.log(err);
+}
+//mongoose.Promise = global.Promise;Password
+
+
