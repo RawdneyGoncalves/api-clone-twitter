9107c779686d89a6c15ab8d40519819ff899e4c6
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2021-12-04 16:39:43 -0300

    Add files via upload

--- database/index.js
+++ database/index.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 try {
- let connect =  mongoose.connect(
+  let connect = mongoose.connect(
     "mongodb+srv://teste:Password@cluster0.3ecyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
 
   );
-  module.exports = connect; 
+  module.exports = connect;
 } catch (err) {
-  console.log(err);
+  console.log("este aqui foi o erro" + err);
 }
-//mongoose.Promise = global.Promise;Password
+
 
 
--- models/user.js
+++ models/user.js
@@ -4,31 +4,30 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
 
-name: {
+    name: {
 
-    type: String, 
-    require: true,
+        type: String,
+        require: true,
 
-}, 
-email: {
-type: String, 
-unique: true, 
-required: true,
-lowercase: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
 
-},
-password: {
-type: String,
-require: true,
-select: false,
-},
-CreatedAt: {
-type: Date,
-default: Date.now,  
-},    
-}); 
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    CreatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
 
-const User = mongoose.model('User', UserSchema); 
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
 
