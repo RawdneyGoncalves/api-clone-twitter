a1b7d1a06ace2ae0eda36a79f048451dea6c58a6
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2021-12-04 16:39:18 -0300

    Add files via upload

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,43 +1,62 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
 const router = express.Router();
-module.exports = { 
-//registrar usuario.  
-  async create (req, res){
-  //console.log(req.body)
-  try {
-    const { email } = req.body;
-    //const usuario = await User.find({ where:{email:email} })
-    //console.log(email)
-    //if (!usuario){
-      //console.log(usuario)
-      //return res.status(400).send({ error: usuario })
+module.exports = {
+  //registrar usuario
+  async create(req, res) {
+    try {
+      const { email } = req.body;
+
+      const user = await User.create({
+        ...req.body,
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(8))
+      });
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).send("Usuario já cadastrado");
+    }
+  },
+
+  async authentication(req, res) {
+    //faz a autenticação
+    const { email, password } = req.body;
+    try {
+    
+      const user = await User.findOne({ email })
+      if (!user) throw new Error('usuario ou senha errada, verifique sua senha e E-mail')  
+      const passwordMatch = bcrypt.compareSync(String(password), user.password); 
+      if (!passwordMatch) throw new Error('usuario ou senha errada, verifique sua senha e E-mail')  
       
-    //};
-    console.log(email)
-    const user = await User.create(req.body);
+      
+      return res.json({ message: "logado, bem-vindo senhor: " + user.name });
 
-    return res.json(user);
-  } catch (err) {
-    return res.status(400).send(err);
+    } catch (error) {
+
+      
+      return res.json({error:error.message})
+
+    }
+
+
+  },
+
+
+  async busca(req, res) {
+    User.findById(req.params.id, function (err, user) {
+      if (err) res.send(err)
+      // retorna o usuário desejado.
+      return res.json(user)
+    });
+
+  },
+
+  async showall(req, res) {
+
+    const users = await User.find()
+    return res.json(users)
   }
-},
-
- async authentication(req, res){
-
-    const {email, password} = req.body; 
-    const user = await User.findOne({email, password})
-    if (!user) return res.status(400).send({error: 'usuario não encontrado'});
-    return res.json({message: "logado"});
-   
-       
-}, 
-
-async showall (req, res){
-
-  const users = await User.find()
-  return res.json(users)
-}
 }
