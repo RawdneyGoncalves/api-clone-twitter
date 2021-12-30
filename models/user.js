889242b2007b45c2570498044abfb3b8652be8fb
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2021-12-30 17:36:06 -0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,34 @@
+
+//const mongoose = require('../database/index');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const UserSchema = new mongoose.Schema({
+
+    name: {
+
+        type: String,
+        require: true,
+
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    CreatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
+
+
