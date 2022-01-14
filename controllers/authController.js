98b908401b7d6bd0bdb2709c5f34d4341de20582
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-01-14 18:35:11 -0300

    ultimas att

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,8 +7,10 @@ const router = express.Router();
 module.exports = {
   //registrar usuario
   async create(req, res) {
+
+    const { email, password } = req.body;
+
     try {
-      const { email, password } = req.body;
 
       const user = await User.create({
         email,
@@ -16,23 +18,29 @@ module.exports = {
       });
 
       return res.json(user);
-    } catch (err) {
-      return res.status(400).send("Usuario já cadastrado");
+
+    } catch (error) {
+
+      return res.status(400).send(`Usuario já cadastrado: ${email}`);
     }
   },
-
+  //faz a autenticação
   async authentication(req, res) {
-    //faz a autenticação
-    const { email, password } = req.body;
+    const { email, password, name } = req.body;
     try {
-      const user = await User.findOne({ email });
+      const user = await User.findOne({ email, name });
       if (!user) res.status(400);
       const passwordMatch = bcrypt.compareSync(String(password), user.password);
-      if (!passwordMatch) res.status(400);
+      if (!passwordMatch)
+        res
+          .status(400)
+          .send(
+            "login ou senha errada, por-favor verifique e tente novamente."
+          );
 
-      return res.json({ message: "logado, bem-vindo senhor: " + user.name });
+      return res.json({ message: "logado, bem-vindo senhor: " + user.email || user.name });
     } catch (error) {
-      return res.json({ error: error.message });
+
     }
   },
 
