4dbebf7761fdf699770975ee8f24045ff55dec95
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-03-22 17:58:57 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ module.exports = {
       return res.status(400).send(`Usuario já cadastrado: ${email}`);
     }
   },
-  //faz a autenticação do usuario
+  //faz a autenticação do usuario e compara
   async authentication(req, res) {
     const { email, password, name } = req.body;
     try {
